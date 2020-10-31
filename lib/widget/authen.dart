fe20fb3cccb9cd7a98593b6fa5e0e09b1e9015ce
masterUNG <phrombutr@gmail.com>
2020-10-31 16:39:28 +0700

    update 31Oct

---
+++ lib/widget/authen.dart
@@ -0,0 +1,231 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:tongmoopa/utlity/normal_dialog.dart';
+import 'package:tongmoopa/widget/main_SOS.dart';
+import 'package:tongmoopa/widget/main_user.dart';
+import 'package:tongmoopa/widget/register.dart';
+
+class Authen extends StatefulWidget {
+  @override
+  _AuthenState createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  String user, password;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkLogin();
+  }
+
+  Future<Null> checkLogin() async {
+    await Firebase.initializeApp().then((value) async {
+      await FirebaseAuth.instance.authStateChanges().listen((event) async {
+        if (event != null) {
+          await FirebaseFirestore.instance
+              .collection('Type')
+              .doc(event.uid)
+              .snapshots()
+              .listen((event) {
+            String type = event.data()['Type'];
+            print('type = $type');
+            routeToService(type);
+          });
+        }
+      });
+    });
+  }
+
+  void routeToService(String type) {
+    switch (type) {
+      case 'User':
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MainUser(),
+            ),
+            (route) => false);
+        break;
+      case 'SOS':
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MainSOS(),
+            ),
+            (route) => false);
+        break;
+      default:
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          // gradient: RadialGradient(
+            // colors: [
+            //   Colors.green.shade100,
+            //   Colors.blue.shade900,
+            // ],
+          //   radius: 1,
+          //   center: Alignment(0, -0.3),
+          // ),
+        ),
+        child: Stack(
+          children: [
+            Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    buildLogo(),
+                    buildText(),
+                    buildUser(),
+                    buildPassword(),
+                    // buildLogin() ???
+                    buildContainer()
+                  ],
+                ),
+              ),
+            ),
+            buildNewRegis()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column buildNewRegis() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlatButton(
+              onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Register(),
+                  )),
+              child: Text(
+                'New register',
+                style: TextStyle(
+                  color: Colors.pink,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Container buildContainer() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: 250,
+      child: RaisedButton(
+        onPressed: () {
+          if (user == null ||
+              user.isEmpty ||
+              password == null ||
+              password.isEmpty) {
+            normalDialod(context, 'Have Space ? Please Fill Every Blank');
+          } else {
+            checkAuthen();
+          }
+        },
+        child: Text(
+          'login',
+          style: TextStyle(
+            color: Colors.yellow.shade100,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<Null> checkAuthen() async {
+    await Firebase.initializeApp().then((value) async {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: user, password: password)
+          .then((value) async {
+        String uid = value.user.uid;
+        await FirebaseFirestore.instance
+            .collection('Type')
+            .doc(uid)
+            .snapshots()
+            .listen((event) {
+          String type = event.data()['Type'];
+
+          routeToService(type);
+        });
+      }).catchError((value) {
+        normalDialod(context, value.message);
+      });
+    });
+  }
+
+// Test???shortkey..
+// class Name { ???
+  // Widget
+// }
+
+// Container ???
+
+  Container buildUser() {
+    return Container(
+      margin: EdgeInsets.only(top: 16, bottom: 16),
+      child: TextField(
+        onChanged: (value) => user = value.trim(),
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'User :',
+          suffixIcon: Icon(Icons.account_circle),
+        ),
+      ),
+    );
+  }
+
+  Container buildPassword() {
+    return Container(
+      child: TextFormField(
+        onChanged: (value) => password = value.trim(),
+        obscureText: true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Password :',
+          suffixIcon: Icon(Icons.remove_red_eye),
+        ),
+      ),
+    );
+  }
+
+  Text buildText() => Text(
+        'MOOPA',
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          // fontStyle: FontStyle.italic,
+          color: Colors.brown.shade500,
+        ),
+      );
+
+  Container buildLogo() {
+    return Container(
+      width: 190,
+      child: Image.asset('images/theone.jpg'),
+    );
+  }
+}
+
+// buildLogin() { ???
+// }
---
+++ lib/widget/register.dart
@@ -0,0 +1,331 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tongmoopa/utlity/normal_dialog.dart';
+import 'package:tongmoopa/model/list_item_str.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  File file;
+
+  String urlAvatar,
+      name,
+      lastName,
+      gender,
+      age,
+      birth,
+      email,
+      password,
+      type = 'User';
+      
+  List<ListItem> _dropdownItems = [
+    ListItem("User", "User"),
+    ListItem("SOS", "SOS"),
+  ];
+
+  List<DropdownMenuItem<ListItem>> _dropdownMenuItems;
+  ListItem _selectedItem;
+
+  void initState() {
+    super.initState();
+    _dropdownMenuItems = buildDropDownMenuItems(_dropdownItems);
+    _selectedItem = _dropdownMenuItems[0].value;
+  }
+
+  List<DropdownMenuItem<ListItem>> buildDropDownMenuItems(List listItems) {
+    List<DropdownMenuItem<ListItem>> items = List();
+    for (ListItem listItem in listItems) {
+      items.add(
+        DropdownMenuItem(
+          child: Text(
+            listItem.name,
+            // style: TextStyle(color: Colors.red),
+          ),
+          value: listItem,
+        ),
+      );
+    }
+    return items;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              buildAvater(),
+              buildCamera(),
+              buildName(),
+              buildLastName(),
+              buildGender(),
+              buildAge(),
+              buildBirth(),
+              buildEmail(),
+              buildPassword(),
+              buildType(),
+              buildRaisedButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container buildRaisedButton() => Container(
+        width: MediaQuery.of(context).size.width,
+        child: RaisedButton.icon(
+          color: Colors.deepOrange,
+          onPressed: () {
+            if (file == null) {
+              normalDialod(context, 'Please chose image');
+            } else if (name == null ||
+                name.isEmpty ||
+                lastName == null ||
+                lastName.isEmpty ||
+                gender == null ||
+                gender.isEmpty ||
+                age == null ||
+                age.isEmpty ||
+                birth == null ||
+                birth.isEmpty ||
+                email == null ||
+                email.isEmpty ||
+                password == null ||
+                password.isEmpty ||
+                type == null ||
+                type.isEmpty) {
+              normalDialod(context, 'Have space ? Please Fill Every Blank');
+            } else {
+              registerThread();
+            }
+          },
+          icon: Icon(
+            Icons.cloud_upload,
+            color: Colors.purple,
+          ),
+          label: Text(
+            'Register',
+            style: TextStyle(color: Colors.yellow),
+          ),
+        ),
+      );
+
+  Container buildCamera() {
+    return Container(
+      width: 250,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            icon: Icon(Icons.add_a_photo),
+            onPressed: () => chooseImage(ImageSource.camera),
+          ),
+          IconButton(
+            icon: Icon(Icons.add_photo_alternate),
+            onPressed: () => chooseImage(ImageSource.gallery),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<Null> chooseImage(ImageSource source) async {
+    try {
+      var result = await ImagePicker().getImage(
+        source: source,
+        maxWidth: 800,
+        maxHeight: 800,
+      );
+      setState(() {
+        file = File(result.path);
+      });
+    } catch (e) {}
+  }
+
+  Container buildAvater() {
+    return Container(
+      width: 250,
+      height: 250,
+      child: file == null ? Image.asset('images/avatar.png') : Image.file(file),
+    );
+  }
+
+  Container buildName() {
+    return Container(
+      margin: EdgeInsets.only(top: 16, bottom: 16),
+      child: TextField(
+        onChanged: (value) => name = value.trim(),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Name :',
+          suffixIcon: Icon(Icons.account_circle),
+        ),
+      ),
+    );
+  }
+
+  Container buildLastName() {
+    return Container(
+      margin: EdgeInsets.only(top: 16, bottom: 16),
+      child: TextField(
+        onChanged: (value) => lastName = value.trim(),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'LastName :',
+          suffixIcon: Icon(Icons.account_circle),
+        ),
+      ),
+    );
+  }
+
+  Container buildGender() {
+    return Container(
+      margin: EdgeInsets.only(top: 16, bottom: 16),
+      child: TextField(
+        onChanged: (value) => gender = value.trim(),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Gender :',
+          suffixIcon: Icon(Icons.wc),
+        ),
+      ),
+    );
+  }
+
+  Container buildAge() {
+    return Container(
+      margin: EdgeInsets.only(top: 16, bottom: 16),
+      child: TextField(
+        keyboardType: TextInputType.number,
+        onChanged: (value) => age = value.trim(),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Age :',
+          suffixIcon: Icon(Icons.nature_people),
+        ),
+      ),
+    );
+  }
+
+  Container buildBirth() {
+    return Container(
+      margin: EdgeInsets.only(top: 16, bottom: 16),
+      child: TextField(
+        onChanged: (value) => birth = value.trim(),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Birth Day :',
+          suffixIcon: Icon(Icons.wb_sunny),
+        ),
+      ),
+    );
+  }
+
+  Container buildEmail() {
+    return Container(
+      margin: EdgeInsets.only(top: 16, bottom: 16),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        onChanged: (value) => email = value.trim(),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'E-mail :',
+          suffixIcon: Icon(Icons.alternate_email),
+        ),
+      ),
+    );
+  }
+
+  Container buildPassword() {
+    return Container(
+      margin: EdgeInsets.only(top: 16, bottom: 16),
+      child: TextField(
+        onChanged: (value) => password = value.trim(),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Password :',
+          suffixIcon: Icon(Icons.lock),
+        ),
+      ),
+    );
+  }
+
+  Container buildType() {
+    return Container(
+      margin: EdgeInsets.only(top: 16, bottom: 16),
+      child: DropdownButton<ListItem>(
+        hint: Text('USER (class People)'),
+        value: _selectedItem,
+        items: _dropdownMenuItems,
+        onChanged: (value) {
+          setState(() {
+            _selectedItem = value;
+            type = value.value.trim();
+          });
+        },
+      ),
+      // child: TextField(
+      //   onChanged: (value) => type = value.trim(),
+      //   decoration: InputDecoration(
+      //     border: OutlineInputBorder(),
+      //     labelText: 'Type :',
+      //     suffixIcon: Icon(Icons.description),
+      //   ),
+      // ),
+    );
+  }
+
+  Future<Null> registerThread() async {
+    await Firebase.initializeApp().then((value) async {
+      print('Connected Success');
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password)
+          .then((value) async {
+        String uid = value.user.uid;
+        print('uid ==>> $uid');
+        StorageReference reference =
+            FirebaseStorage.instance.ref().child('Avartar/$uid.jpg');
+        StorageUploadTask task = reference.putFile(file);
+        String urlAvatar = await (await task.onComplete).ref.getDownloadURL();
+        print('urlAvatar = $urlAvatar');
+        Map<String, dynamic> typeData = Map();
+        typeData['Type'] = type;
+        await FirebaseFirestore.instance
+            .collection('Type')
+            .doc(uid)
+            .set(typeData);
+
+        Map<String, dynamic> userdata = Map();
+        userdata['UrlAvatar'] = urlAvatar;
+        userdata['Name'] = name;
+        userdata['LastName'] = lastName;
+        userdata['Gender'] = gender;
+        userdata['Age'] = age;
+        userdata['Birth'] = birth;
+        userdata['Email'] = email;
+        userdata['Password'] = password;
+        userdata['Type'] = type;
+
+        await FirebaseFirestore.instance
+            .collection('User')
+            .doc(uid)
+            .set(userdata)
+            .then((value) => Navigator.pop(context));
+      });
+    }).catchError((value) {
+      normalDialod(context, value.message);
+    });
+  }
+}
